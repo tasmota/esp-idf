029dde5a12f7fffed80adce4705e8815db21f751
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-02-05 15:57:48 +0100

    Revert "update 05022022"

--- components/wifi_provisioning/src/manager.c
+++ components/wifi_provisioning/src/manager.c
@@ -1,8 +1,16 @@
-/*
- * SPDX-FileCopyrightText: 2019-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+// Copyright 2019 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <string.h>
 #include <sys/param.h>
@@ -615,10 +623,8 @@ static bool wifi_prov_mgr_stop_service(bool blocking)
          * released - some duration after - returning from a call to
          * wifi_prov_mgr_stop_provisioning(), like when it is called
          * inside a protocomm handler */
-        if (xTaskCreate(prov_stop_task, "prov_stop_task", 4096, (void *)1, tskIDLE_PRIORITY, NULL) != pdPASS) {
-            ESP_LOGE(TAG, "Failed to create prov_stop_task!");
-            abort();
-        }
+        assert(xTaskCreate(prov_stop_task, "prov_stop_task", 4096, (void *)1,
+                           tskIDLE_PRIORITY, NULL) == pdPASS);
         ESP_LOGD(TAG, "Provisioning scheduled for stopping");
     }
     return true;

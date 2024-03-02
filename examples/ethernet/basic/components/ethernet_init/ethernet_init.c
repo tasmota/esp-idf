6cef53544527fb0a2025a93eeba93fd43011c552
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-03-02 13:54:31 +0100

    Update ethernet_init.c

--- examples/ethernet/basic/components/ethernet_init/ethernet_init.c
+++ examples/ethernet/basic/components/ethernet_init/ethernet_init.c
@@ -71,11 +71,6 @@ static esp_eth_handle_t eth_init_internal(esp_eth_mac_t **mac_out, esp_eth_phy_t
     // Update vendor specific MAC config based on board configuration
     esp32_emac_config.smi_mdc_gpio_num = CONFIG_EXAMPLE_ETH_MDC_GPIO;
     esp32_emac_config.smi_mdio_gpio_num = CONFIG_EXAMPLE_ETH_MDIO_GPIO;
-#if CONFIG_EXAMPLE_USE_SPI_ETHERNET
-    // The DMA is shared resource between EMAC and the SPI. Therefore, adjust
-    // EMAC DMA burst length when SPI Ethernet is used along with EMAC.
-    esp32_emac_config.dma_burst_len = ETH_DMA_BURST_LEN_4;
-#endif // CONFIG_EXAMPLE_USE_SPI_ETHERNET
     // Create new ESP32 Ethernet MAC instance
     esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&esp32_emac_config, &mac_config);
     // Create new PHY instance based on board configuration

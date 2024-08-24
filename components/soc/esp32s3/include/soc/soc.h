d1e97f1353da8611df8df876b92de9adc0c6f8f9
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-24 11:01:15 +0200

    Tasmota changes (
    * Delete components/mqtt directory
    * Delete components/spiffs directory
    * add "SOC_ADC_DIGI_RESULT_BYTES" to C2 soc_caps.h
    * add jl1101 PHY
    * add xiaomi solo1
    * do not abort when no PSRAM found
    * Correct REG_SPI_BASE(i)
    * Fix Audio DAC
    * add missing  '.*.o'
    * Delete components/wifi_provisioning directory

--- components/soc/esp32s3/include/soc/soc.h
+++ components/soc/esp32s3/include/soc/soc.h
@@ -34,7 +34,7 @@
 #define REG_I2S_BASE( i )       (DR_REG_I2S_BASE + (i) * 0x1E000)
 #define REG_TIMG_BASE(i)        (DR_REG_TIMERGROUP0_BASE + (i)*0x1000)
 #define REG_SPI_MEM_BASE(i)     (DR_REG_SPI0_BASE - (i) * 0x1000)
-#define REG_SPI_BASE(i)         (((i)>=2) ? (DR_REG_SPI2_BASE + (i-2) * 0x1000) : (0))    // GPSPI2 and GPSPI3
+#define REG_SPI_BASE(i)         (((i)==2) ? (DR_REG_SPI2_BASE) : (DR_REG_SPI0_BASE - ((i) * 0x1000)))  // GPSPI2 and GPSPI3
 #define REG_I2C_BASE(i)         (DR_REG_I2C_EXT_BASE + (i) * 0x14000 )
 
 //Convenient way to replace the register ops when ulp riscv projects
@@ -156,6 +156,7 @@
 #define  CPU_CLK_FREQ_MHZ_BTLD                       (80)               // The cpu clock frequency (in MHz) to set at 2nd stage bootloader system clock configuration
 #define  CPU_CLK_FREQ                                APB_CLK_FREQ
 #define  APB_CLK_FREQ                                (80*1000000)
+#define  MODEM_REQUIRED_MIN_APB_CLK_FREQ             (80*1000000)
 #define  REF_CLK_FREQ                                (1000000)
 #define  XTAL_CLK_FREQ                               (40*1000000)
 #define  UART_CLK_FREQ                               APB_CLK_FREQ

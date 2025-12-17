f96119db753891b515e26bc508eaaa8f19565c3d
Aditya Patwardhan <aditya.patwardhan@espressif.com>
2025-12-17 17:06:39 +0530

    feat(esp-tls): Added support to register custom tls stack
        * Removed the esp_tls_wolfssl layer from esp-tls
        * Migrated Error codes

--- components/esp-tls/esp-tls-crypto/esp_tls_crypto.c
+++ components/esp-tls/esp-tls-crypto/esp_tls_crypto.c
@@ -8,21 +8,12 @@
 #include "esp_log.h"
 #include "esp_err.h"
 #include "sdkconfig.h"
-__attribute__((unused)) static const char *TAG = "esp_crypto";
-#ifdef CONFIG_ESP_TLS_USING_MBEDTLS
 #include "mbedtls/base64.h"
 #include "mbedtls/error.h"
 #include "psa/crypto.h"
-#define _esp_crypto_sha1 esp_crypto_sha1_mbedtls
-#define _esp_crypto_base64_encode esp_crypto_bas64_encode_mbedtls
-#elif  CONFIG_ESP_TLS_USING_WOLFSSL
-#include "wolfssl/ssl.h" /* SHA functions are listed in wolfssl/ssl.h */
-#include "wolfssl/wolfcrypt/coding.h"
-#define _esp_crypto_sha1 esp_crypto_sha1_wolfSSL
-#define _esp_crypto_base64_encode esp_crypto_base64_encode_woflSSL
-#endif
 
-#ifdef CONFIG_ESP_TLS_USING_MBEDTLS
+__attribute__((unused)) static const char *TAG = "esp_crypto";
+
 static int esp_crypto_sha1_mbedtls( const unsigned char *input,
                                     size_t ilen,
                                     unsigned char output[20])
@@ -60,39 +51,15 @@ static int esp_crypto_bas64_encode_mbedtls( unsigned char *dst, size_t dlen,
     return mbedtls_base64_encode(dst, dlen, olen, src, slen);
 }
 
-#elif CONFIG_ESP_TLS_USING_WOLFSSL
-static int esp_crypto_sha1_wolfSSL( const unsigned char *input,
-                                    size_t ilen,
-                                    unsigned char output[20])
-{
-    unsigned char *ret = wolfSSL_SHA1(input, ilen, output);
-    if (ret == NULL) {
-        ESP_LOGE(TAG, "Error in calculating sha1 sum");
-        return -1;
-    }
-    return 0;
-}
-
-static int esp_crypto_base64_encode_woflSSL(unsigned char *dst, size_t dlen, size_t *olen,
-        const unsigned char *src, size_t slen)
-{
-    *olen = dlen;
-    return Base64_Encode_NoNl((const byte *) src, (word32) slen, (byte *) dst, (word32 *) olen);
-}
-
-#else
-#error "No TLS/SSL Stack selected"
-#endif
-
 int esp_crypto_sha1( const unsigned char *input,
                      size_t ilen,
                      unsigned char output[20])
 {
-    return _esp_crypto_sha1(input, ilen, output);
+    return esp_crypto_sha1_mbedtls(input, ilen, output);
 }
 
 int esp_crypto_base64_encode(unsigned char *dst, size_t dlen, size_t *olen,
                              const unsigned char *src, size_t slen )
 {
-    return _esp_crypto_base64_encode(dst, dlen, olen, src, slen);
+    return esp_crypto_bas64_encode_mbedtls(dst, dlen, olen, src, slen);
 }

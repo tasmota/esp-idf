f2a3fa2b49d58267971a4d473b76235ae592e6f6
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-04-09 23:18:06 +0200

    Tasmota changes

--- components/soc/project_include.cmake
+++ components/soc/project_include.cmake
@@ -13,10 +13,11 @@ if(CONFIG_IDF_TOOLCHAIN_GCC)
            CONFIG_IDF_TARGET_ESP32C61 OR
            CONFIG_IDF_TARGET_ESP32H2 OR
            CONFIG_IDF_TARGET_ESP32H21)
-        set(_march "rv32imac_zicsr_zifencei")
-    elseif(CONFIG_IDF_TARGET_ESP32H4 OR
-           CONFIG_IDF_TARGET_ESP32P4)
-        set(_march "rv32imafc_zicsr_zifencei")
+        set(_march "rv32imac_zicsr_zifencei_zaamo_zalrsc")
+    elseif(CONFIG_IDF_TARGET_ESP32H4)
+        set(_march "rv32imafcb_zicsr_zifencei_zaamo_zalrsc")
+    elseif(CONFIG_IDF_TARGET_ESP32P4 OR CONFIG_IDF_TARGET_ESP32S31)
+        set(_march "rv32imafc_zicsr_zifencei_zaamo_zalrsc")
     elseif(NOT(CONFIG_IDF_TARGET_ESP32S2 OR CONFIG_IDF_TARGET_ESP32S3))
         message(FATAL_ERROR "Unknown Espressif target: ${CONFIG_IDF_TARGET}")
     endif()
@@ -33,18 +34,41 @@ if(CONFIG_IDF_TOOLCHAIN_GCC)
         endif()
 
         # Clean compile options that were added by previous configurations and may be outdated
-        idf_toolchain_remove_flags(COMPILE_OPTIONS "-march=")
+        idf_toolchain_remove_flags(COMPILE_OPTIONS "-march="
+                                                   "-mno-cm-push-reverse"
+                                                   "-mno-cm-popret")
 
-        if(CONFIG_ESP32P4_SELECTS_REV_LESS_V3)
-            set(_march "${_march}_xesppie")
-        else()
-            if(CONFIG_SOC_CPU_HAS_HWLOOP)
-                set(_march "${_march}_xesploop")
-            endif()
+        if(CONFIG_SOC_CPU_HAS_ZB_EXTENSIONS)
+            set(_march "${_march}_zba_zbb_zbs")
+        endif()
 
-            if(CONFIG_SOC_CPU_HAS_PIE)
-                set(_march "${_march}_xespv")
+        if((CONFIG_SOC_CPU_HAS_ZC_EXTENSIONS AND NOT CONFIG_SOC_CPU_ZCMP_WORKAROUND) OR
+           CONFIG_COMPILER_ENABLE_RISCV_ZCMP)
+            if(NOT CONFIG_ESP32P4_SELECTS_REV_LESS_V3)
+                set(_march "${_march}_zcb_zcmp_zcmt")
+
+                if(CONFIG_SOC_CPU_ZCMP_PUSH_REVERSED)
+                    idf_toolchain_add_flags(COMPILE_OPTIONS "-mno-cm-push-reverse")
+                endif()
+                if(CONFIG_SOC_CPU_ZCMP_POPRET_ISSUE)
+                    idf_toolchain_add_flags(COMPILE_OPTIONS "-mno-cm-popret")
+                endif()
             endif()
+        endif()
+
+        if(CONFIG_SOC_CPU_HAS_HWLOOP)
+            set(_march "${_march}_xesploop")
+        endif()
+
+        if(CONFIG_SOC_CPU_HAS_PIE)
+            set(_march "${_march}_xespv")
+            if(CONFIG_ESP32P4_SELECTS_REV_LESS_V3)
+                set(_march "${_march}2p1")
+            endif()
+        endif()
+
+        if(CONFIG_SOC_CPU_HAS_DSP)
+            set(_march "${_march}_xespdsp")
         endif()
 
         # Set ABI and ARCH options
@@ -53,6 +77,9 @@ if(CONFIG_IDF_TOOLCHAIN_GCC)
         endif()
         idf_toolchain_add_flags(COMPILE_OPTIONS "-march=${_march}")
 
+        if(NOT CONFIG_SOC_CPU_MISALIGNED_ACCESS_ON_PMP_MISMATCH_ISSUE)
+            idf_toolchain_add_flags(COMPILE_OPTIONS "-mtune=esp-base")
+        endif()
         idf_toolchain_rerun_abi_detection()
     else()
         message(FATAL_ERROR "Unknown Espressif architecture: ${CONFIG_IDF_TARGET_ARCH}")
